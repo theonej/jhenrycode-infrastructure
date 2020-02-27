981322ef2f12b3b00d4ea3f87de7841950479d60
ultralord <j.henry@n-dexed.com>
2020-02-27 15:10:56 -0500

    updating security group port to allow for health check to succeed

--- definitions/security.tf
+++ definitions/security.tf
@@ -5,8 +5,8 @@ resource "aws_security_group" "jhenrycode-vision" {
 
     ingress {
         protocol = "tcp"
-        from_port = 80
-        to_port = 80
+        from_port = 9001
+        to_port = 9001
         cidr_blocks = ["0.0.0.0/0"]
     }
 
